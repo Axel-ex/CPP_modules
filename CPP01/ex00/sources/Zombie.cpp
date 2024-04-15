c07810a2e2ed54b3e61f709f703136bfc6a3a84e
Axel chabrerie <114242486+Axel-ex@users.noreply.github.com>
2024-04-15 14:37:26 +0100

    add destructor msg cpp01/ex00

--- CPP01/ex00/sources/Zombie.cpp
+++ CPP01/ex00/sources/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: achabrer <achabrer@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 15:19:26 by achabrer          #+#    #+#             */
-/*   Updated: 2024/01/26 16:25:31 by achabrer         ###   ########.fr       */
+/*   Updated: 2024/04/15 14:24:49 by Axel             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 Zombie::Zombie() {}
 
-Zombie::~Zombie() {}
+Zombie::~Zombie()
+{
+	std::cout << "Zombie " << _name << " destructed" << std::endl;
+}
 
 Zombie::Zombie(std::string name) : _name(name) {}
 
